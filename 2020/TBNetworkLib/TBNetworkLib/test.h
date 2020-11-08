438b18c6279ad614b85b15d684677e3d0c915fc5
true-bear <jazzmaster0415@gmail.com>
2020-11-08 15:25:07 +0900

    new 2020 project

---
+++ 2020/TBNetworkLib/TBNetworkLib/test.h
@@ -0,0 +1,14 @@
+#pragma once
+
+
+#include <iostream>
+
+class Test
+{
+public:
+
+	void PrintTest()
+	{
+		std::cout << "Áø¼ÒÀ± ¹Ùº¸" << std::endl;
+	}
+};
---
+++ 2020/TBServer/GameSvr/main.cpp
@@ -0,0 +1,15 @@
+
+#include "../../TBNetworkLib/TBNetworkLib/test.h"
+
+
+
+int main()
+{
+
+	Test tt;
+	tt.PrintTest();
+
+	return 0;
+}
+
+
